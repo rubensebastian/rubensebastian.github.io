a73f4878adf9e2841fbbb01db1c72361ac7e4fe2
Sebastian <ruben.sebastian.aleman@gmail.com>
2023-09-09 20:35:31 -0400

    updated left/right mechanism

--- sketch.js
+++ sketch.js
@@ -34,9 +34,11 @@ let currentlyJumping = false
 let gameRunning = false
 let speed = 3//update based on how long the game has gone on
 
-let platforms = []
-platforms.push(new Platform(50, 150, 760), new Platform(250, 350, 660), new Platform(450, 550, 560))
-let circleJumper = new Jumper(150, 750, 20)
+let platforms = [new Platform(50, 150, 760), new Platform(250, 350, 710), new Platform(450, 550, 660)]
+let circleJumper = new Jumper(100, 750, 20)
+let currentPlatform = 0
+
+let instruction = 'Press "a" to go left, "d" to go right, and the spacebar to jump'
 
 function setup() {
     createCanvas(windowWidth - 100, windowHeight - 100)
@@ -46,15 +48,21 @@ function setup() {
 function draw() {
     background(75)
     circle(circleJumper.xPosition, circleJumper.yPosition, circleJumper.diameter)
+    textSize(20)
+    fill(255)
+    text(instruction, 10, 10, 250, 250)
 
     for (let platform of platforms) {
         line(platform.left, platform.height, platform.right, platform.height)
     }
 
     if (keyIsDown(65) && circleJumper.xPosition - 10 > 0) {//press "a" to go left
-        circleJumper.moveLeft(3)
+        if (currentlyJumping || circleJumper.xPosition - 3 > platforms[currentPlatform].left) {
+            circleJumper.moveLeft(3)
+        }
     }
     if (keyIsDown(68) && circleJumper.xPosition + 10 < windowWidth - 100) {//press "d" to go right
+        if(currentlyJumping || circleJumper.xPosition + 3 < platforms[currentPlatform].right)
         circleJumper.moveRight(3)
     }
 }
@@ -88,9 +96,10 @@ function motionOfArc() {
 }
 
 function circleLands(counter, nextHeight) {
-    for (let platform of platforms) {
-        if (counter > 121 && platform.left <= circleJumper.xPosition && circleJumper.xPosition <= platform.right && circleJumper.yPosition + 10 <= platform.height && nextHeight + 10 > platform.height) {
-            return platform.height - 10
+    for (let index = 0; index < platforms.length; index++) {
+        if (counter > 121 && platforms[index].left <= circleJumper.xPosition && circleJumper.xPosition <= platforms[index].right && circleJumper.yPosition + 10 <= platforms[index].height && nextHeight + 10 > platforms[index].height) {
+            currentPlatform = index
+            return platforms[index].height - 10
         }
     }
     return false
